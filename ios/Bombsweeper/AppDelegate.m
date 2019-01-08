b9dc15631a88426e97f6e9eb6c461ec048f1fafc
Armen Badalyan <flexer87@gmail.com>
2019-01-08 20:40:45 +0300

    squash! splash screen and icon

--- ios/Bombsweeper/AppDelegate.m
+++ ios/Bombsweeper/AppDelegate.m
@@ -19,7 +19,11 @@
  [FIRApp configure];
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  #ifdef DEBUG
+    jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  #else
+    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  #endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"Bombsweeper"
